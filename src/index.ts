4175ae2739ff38bfdf8a7705672220500d9f0dc3
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2021-11-13 11:11:40 +1100

    feat: set up custome map

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+/// <reference types="@types/google.maps" />
 import { User } from "./User";
 import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 
 const user = new User();
 const company = new Company();
+const customMap = new CustomMap("#map");
 
-console.log(user);
-console.log(company);
+customMap.addMarker(user);
+customMap.addMarker(company);
