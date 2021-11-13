49665db43a30a957781d905cc0705aeb27c69163
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2021-11-13 11:11:54 +1100

    feat: apply new compatible interface for User and Company class

--- src/Company.ts
+++ src/Company.ts
@@ -1,6 +1,7 @@
 import faker from "faker";
+import { Mappable } from "./CustomMap";
 
-export class Company {
+export class Company implements Mappable {
   companyName: string;
   catchPhrase: string;
   location: {
@@ -14,5 +15,8 @@ export class Company {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude()),
     };
+  }
+  markerContent(): string {
+    return this.companyName;
   }
 }
